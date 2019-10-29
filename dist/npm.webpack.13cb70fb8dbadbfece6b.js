9fd3471a0bb8387f7235af32956f4a8ee61ac340
mert <mrtovun@gmail.com>
2019-10-29 20:38:10 +0300

    bundle splitting

---
+++ dist/npm.webpack.13cb70fb8dbadbfece6b.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],[,function(n,o){var t;t=function(){return this}();try{t=t||new Function("return this")()}catch(n){"object"==typeof window&&(t=window)}n.exports=t}]]);
+//# sourceMappingURL=npm.webpack.13cb70fb8dbadbfece6b.js.map
